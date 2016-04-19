ba070311a68fa2bf914c9b9e1440b7bcf5b52df6
ttoth <tibor.toth@emarsys.com>
2016-04-19 12:30:04 +0200

    Revert: Add headers to signed request

--- src/main/scala/com/emarsys/escher/akka/http/EscherDirectives.scala
+++ src/main/scala/com/emarsys/escher/akka/http/EscherDirectives.scala
@@ -1,5 +1,7 @@
 package com.emarsys.escher.akka.http
 
+import java.util
+
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.client.RequestBuilding
 import akka.http.scaladsl.model.{HttpHeader, HttpRequest}
@@ -12,6 +14,7 @@ import com.emarsys.escher.EscherException
 import spray.json._
 
 import scala.collection.JavaConversions._
+import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Try, Failure, Success}
 
@@ -36,7 +39,7 @@ trait EscherDirectives extends RequestBuilding with EscherAuthenticator {
         escherConfig.secret(serviceName),
         defaultSignedHeaders.union(headers.map(_.name))
       )
-      escherRequest.getHttpRequest.addHeaders(headers)
+      escherRequest.getHttpRequest
     }
   }
 
